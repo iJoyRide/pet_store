0d9ebe97e3cda4b4b30ce220f532306ce5c79e3b
iJoyRide <sumfeis3@gmail.com>
2024-04-14 01:53:26 +0800

    migration

--- backend/backend/Context/ApplicationDbContext.cs
+++ backend/backend/Context/ApplicationDbContext.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using backend.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.Context
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
 
+        }
+
+        public DbSet<ProductEntity> Products { get; set; }
     }
+
+    
+
 }
