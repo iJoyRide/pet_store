b725e9f256eaf600989b866ad6eccd367212c77b
iJoyRide <sumfeis3@gmail.com>
2024-04-14 16:25:47 +0800

    frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div>
+      {/* Navbar */}
+
+      {/* Wrapper  */}
+      <div className="wrapper">
+        {/* Routes */}
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import './navbar.scss'
+import {} from "@mui"
+
+const Navbar: React.FC = () => {
+  return (
+    <div>
+        <div className="brand">Pet Store</div>
+        <div className="hamburger"></div>
+        <div className="menu"></div>
+    </div>
+  )
+}
+
+export default Navbar
